e71f6a957dda6b6fe0b64a9fa013dadbd254e082
Ubuntu <ubuntu@ip-172-31-72-183.ec2.internal>
2025-02-16 01:01:58 +0000

    FIX stage issue

--- api/routes/books.py
+++ api/routes/books.py
@@ -1,6 +1,6 @@
 from typing import OrderedDict
 
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
 
 from api.db.schemas import Book, Genre, InMemoryDB
@@ -32,6 +32,15 @@ db.books = {
     ),
 }
 
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book_by_id(book_id: int):
+    book = db.books.get(book_id)
+    if not book:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"detail": "Book not found"},
+        )
+    return book
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_book(book: Book):
@@ -41,7 +50,9 @@ async def create_book(book: Book):
     )
 
 
-@router.get("/", response_model=OrderedDict[int, Book], status_code=status.HTTP_200_OK)
+@router.get(
+    "/", response_model=OrderedDict[int, Book], status_code=status.HTTP_200_OK
+)
 async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
@@ -58,15 +69,3 @@ async def update_book(book_id: int, book: Book) -> Book:
 async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
-
-
-@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
-async def get_book(book_id: int):
-    book = db.books.get(book_id)
-    if not book:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Book not found"
-        )
-    return book
-
